19832a259c41c99644eba97c20643ffd97aaca40
nichwch <nichwch@gmail.com>
2024-02-05 14:06:59 -0500

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let arr = [];
+</script>
+
+<div class="w-[45rem] mx-auto mt-24 h-[30rem] border-2 border-black bg-red-400 flex flex-col">
+	<div class="flex flex-grow border-b-2 border-black">
+		<div class=" flex-grow p-3 border-r-2 border-black">hello</div>
+		<div class=" flex-grow p-3">hello</div>
+	</div>
+	<div class="p-3">
+		<button class="button block bg-red-200 hover:bg-red-300">add random number</button>
+		<button class="mt-3 button block bg-red-200 hover:bg-red-300">pop root</button>
+	</div>
+</div>
